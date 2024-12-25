ee70b59c088e01717ae84764d026ca49d16fce59
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2024-12-25 18:14:14 -0500

    Draw individual elements on Canvas still working on removing button

--- JavaScript.js
+++ JavaScript.js
@@ -1,220 +1,177 @@
-var circleStart = 0;
-var circleEnd = Math.PI * 2;
+const circleStart = 0,
+  circleEnd = Math.PI * 2;
 
-function begin(context) {
-  context.beginPath();
-}
+const draw = (context, fn) => {
+  context[fn]();
+};
 
-function close(context) {
-  context.closePath();
-}
-
-function stroke(context) {
-  context.stroke();
-}
-
-function fill(context) {
-  context.fill();
-}
-
-function enterRadius() {
-  return (document.getElementById("message").innerHTML = "Enter Radius");
-}
-
-function notInRange() {
-  return (document.getElementById("message").innerHTML =
-    "Number is not in range");
-}
-
-function empty() {
-  return (document.getElementById("message").innerHTML = "");
-}
-
-function drawEmojiOutline(context, size, x, y) {
-  begin(context);
+const drawEmojiOutline = (context, size, x, y) => {
+  draw(context, "beginPath");
   context.arc(x, y, size, circleStart, circleEnd);
-  stroke(context);
-  close(context);
-}
+  draw(context, "stroke");
+  draw(context, "closePath");
+};
 
 function eyes(context, size, x, y, eyeType, position) {
-  var eyeX = size / 2.6;
-  var eyeY = size / 5;
-  var eyeSize = size / 9;
+  const eyeX = size / 2.6,
+    eyeY = size / 5,
+    eyeSize = size / 9;
 
-  begin(context);
-
-  var eyeArcStarts, eyeArcEnds, fillOrStroke;
+  let eyeArcStarts, eyeArcEnds, fillOrStroke;
 
   if (eyeType === "blink") {
-    eyeArcStarts = 3;
-    eyeArcEnds = 6.5;
-    fillOrStroke = "stroke";
+    [eyeArcStarts, eyeArcEnds, fillOrStroke] = [3, 6.5, "stroke"];
   } else if (eyeType === "sad") {
-    eyeArcStarts = circleStart;
-    eyeArcEnds = 3;
-    fillOrStroke = "stroke";
+    [eyeArcStarts, eyeArcEnds, fillOrStroke] = [circleStart, 3, "stroke"];
   } else if (eyeType === "half") {
-    if (position === "left") {
-      eyeArcStarts = 0.5;
-      eyeArcEnds = 3.6;
-    } else if (position === "right") {
-      eyeArcStarts = 6;
-      eyeArcEnds = 2.8;
-    }
+    [eyeArcStarts, eyeArcEnds] = position === "left" ? [0.5, 3.6] : [6, 2.8];
     fillOrStroke = "fill";
   } else if (eyeType === "extraSad") {
-    if (position === "left") {
-      eyeArcStarts = 5.8;
-      eyeArcEnds = 3;
-    } else if (position === "right") {
-      eyeArcStarts = 6.5;
-      eyeArcEnds = 3.6;
-    }
+    [eyeArcStarts, eyeArcEnds] = position === "left" ? [5.8, 3] : [6.5, 3.6];
     fillOrStroke = "fill";
   } else {
-    eyeArcStarts = circleStart;
-    eyeArcEnds = circleEnd;
-    fillOrStroke = "fill";
+    [eyeArcStarts, eyeArcEnds, fillOrStroke] = [circleStart, circleEnd, "fill"];
   }
 
-  if (position === "left") {
-    context.arc(x - eyeX, y - eyeY, eyeSize, eyeArcStarts, eyeArcEnds);
-    fillOrStroke === "fill" ? fill(context) : stroke(context);
-  } else if (position === "right") {
-    context.arc(x + eyeX, y - eyeY, eyeSize, eyeArcStarts, eyeArcEnds);
-    fillOrStroke === "fill" ? fill(context) : stroke(context);
-  }
-
-  close(context);
+  draw(context, "beginPath");
+  context.arc(
+    x + (position === "left" ? -eyeX : eyeX),
+    y - eyeY,
+    eyeSize,
+    eyeArcStarts,
+    eyeArcEnds
+  );
+  draw(context, fillOrStroke);
+  draw(context, "closePath");
 }
 
 function mouth(context, size, x, y, mouthType) {
-  var mouthY = 0;
-  var mouthSize = size / 1.6;
-  var sadY = size / 1;
-  var happyY = size / 8;
-  var blankXY = size / 2.5;
-  var oY = size / 2.5;
-  var oSize = size / 5;
+  var mouthSize = size / 1.6,
+    sadY = size / 1,
+    happyY = size / 8,
+    blankXY = size / 2.5,
+    oY = size / 2.5,
+    oSize = size / 5;
 
-  begin(context);
+  draw(context, "beginPath");
 
   if (mouthType === "smile") {
-    context.arc(x, y + mouthY, mouthSize, 0.9, 2.3);
-    stroke(context);
+    context.arc(x, y, mouthSize, 0.9, 2.3);
+    draw(context, "stroke");
   } else if (mouthType === "happy") {
     context.moveTo(x - mouthSize, y + happyY);
     context.lineTo(x + mouthSize, y + happyY);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.arc(x, y + happyY, mouthSize, circleStart, 3.1);
-    stroke(context);
+    draw(context, "stroke");
   } else if (mouthType === "sad") {
     context.arc(x, y + sadY, mouthSize, 4.1, -0.9);
-    stroke(context);
+    draw(context, "stroke");
   } else if (mouthType === "blank") {
     context.moveTo(x - blankXY, y + blankXY);
     context.lineTo(x + blankXY, y + blankXY);
-    stroke(context);
+    draw(context, "stroke");
   } else if (mouthType === "o") {
     context.arc(x, y + oY, oSize, circleStart, circleEnd);
-    stroke(context);
+    draw(context, "stroke");
   }
 
-  close(context);
+  draw(context, "closePath");
 }
 
-function tougue(context, size, x, y) {
-  var tougueMoveLineX = size / 5;
-  var touguemoveY = size / 1.7;
-  var tougueLineY = size / 1.4;
-  var tougueY = size / 1.4;
-  var tougueSize = size / 5;
+function drawTougue(context, size, x, y, type) {
+  var tougueMoveLineX = size / 5,
+    touguemoveY = size / 1.7,
+    tougueLineY = size / 1.4,
+    tougueY = size / 1.4,
+    tougueSize = size / 5;
 
-  begin(context);
+  if (type === "straight") {
+    // Draw straight tongue
+    draw(context, "beginPath");
 
-  context.moveTo(x + tougueMoveLineX, y + touguemoveY);
-  context.lineTo(x + tougueMoveLineX, y + tougueLineY);
-  stroke(context);
+    context.moveTo(x + tougueMoveLineX, y + touguemoveY);
+    context.lineTo(x + tougueMoveLineX, y + tougueLineY);
+    draw(context, "stroke");
 
-  close(context);
-  begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
-  context.moveTo(x - tougueMoveLineX, y + touguemoveY);
-  context.lineTo(x - tougueMoveLineX, y + tougueLineY);
-  stroke(context);
+    context.moveTo(x - tougueMoveLineX, y + touguemoveY);
+    context.lineTo(x - tougueMoveLineX, y + tougueLineY);
+    draw(context, "stroke");
 
-  close(context);
-  begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
-  context.arc(x, y + tougueY, tougueSize, circleStart, 3);
-  stroke(context);
+    context.arc(x, y + tougueY, tougueSize, circleStart, 3);
+    draw(context, "stroke");
 
-  close(context);
-}
+    draw(context, "closePath");
+  } else if (type === "side") {
+    // Draw side tongue
+    var sideTougueX = size / 5;
+    var sideTougueY = size / 1.7;
 
-function sidetougue(context, size, x, y) {
-  var sideTougueX = size / 5;
-  var sideTougueY = size / 1.7;
-  var tougueSize = size / 5;
+    draw(context, "beginPath");
 
-  begin(context);
+    context.arc(x + sideTougueX, y + sideTougueY, tougueSize, -0.5, 2.9);
+    draw(context, "stroke");
 
-  context.arc(x + sideTougueX, y + sideTougueY, tougueSize, -0.5, 2.9);
-  stroke(context);
-
-  close(context);
+    draw(context, "closePath");
+  } else {
+    console.error("Invalid type specified: " + type);
+  }
 }
 
 function laughTear(context, size, x, y, position) {
-  var laughTearMoveX = size / 2;
-  var laughTearMoveY = size / 9;
-  var laughTearLineX1 = size / 1;
-  var laughTearLineY1 = size / 6;
-  var laughTearLineX2 = size / 1.3;
-  var laughTearLineY2 = size / 2.5;
+  var laughTearMoveX = size / 2,
+    laughTearMoveY = size / 9,
+    laughTearLineX1 = size / 1,
+    laughTearLineY1 = size / 6,
+    laughTearLineX2 = size / 1.3,
+    laughTearLineY2 = size / 2.5,
+    laughTearArcX = size / 1.1,
+    laughTearArcY = size / 3.2,
+    laughTearArcSize = size / 6;
 
-  var laughTearArcX = size / 1.1;
-  var laughTearArcY = size / 3.2;
-  var laughTearArcSize = size / 6;
-
-  begin(context);
+  draw(context, "beginPath");
 
   if (position === "left") {
     context.moveTo(x - laughTearMoveX, y - laughTearMoveY);
     context.lineTo(x - laughTearLineX1, y + laughTearLineY1);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.moveTo(x - laughTearMoveX, y - laughTearMoveY);
     context.lineTo(x - laughTearLineX2, y + laughTearLineY2);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.arc(x - laughTearArcX, y + laughTearArcY, laughTearArcSize, 0.5, 4);
-    stroke(context);
+    draw(context, "stroke");
   } else if (position === "right") {
     context.moveTo(x + laughTearMoveX, y - laughTearMoveY);
     context.lineTo(x + laughTearLineX1, y + laughTearLineY1);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.moveTo(x + laughTearMoveX, y - laughTearMoveY);
     context.lineTo(x + laughTearLineX2, y + laughTearLineY2);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.arc(
       x + laughTearArcX,
@@ -223,39 +180,38 @@ function laughTear(context, size, x, y, position) {
       -1,
       2.5
     );
-    stroke(context);
+    draw(context, "stroke");
   }
 
-  close(context);
+  draw(context, "closePath");
 }
 
 function tear(context, size, x, y, position) {
-  var tearMoveX = size / 2;
-  var tearMoveY = size / 10;
-  var tearLineX1 = size / 1.5;
-  var tearLineX2 = size / 3;
-  var tearLineY = size / 3;
+  var tearMoveX = size / 2,
+    tearMoveY = size / 10,
+    tearLineX1 = size / 1.5,
+    tearLineX2 = size / 3,
+    tearLineY = size / 3,
+    tearCircleX = size / 2,
+    tearCircleY = size / 3,
+    tearCircleSize = size / 6;
 
-  var tearCircleX = size / 2;
-  var tearCircleY = size / 3;
-  var tearCircleSize = size / 6;
-
-  begin(context);
+  draw(context, "beginPath");
 
   if (position === "left") {
     context.moveTo(x - tearMoveX, y - tearMoveY);
     context.lineTo(x - tearLineX1, y + tearLineY);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.moveTo(x - tearMoveX, y - tearMoveY);
     context.lineTo(x - tearLineX2, y + tearLineY);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.arc(
       x - tearCircleX,
@@ -264,21 +220,21 @@ function tear(context, size, x, y, position) {
       circleStart,
       3.2
     );
-    stroke(context);
+    draw(context, "stroke");
   } else if (position === "right") {
     context.moveTo(x + tearMoveX, y - tearMoveY);
     context.lineTo(x + tearLineX1, y + tearLineY);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.moveTo(x + tearMoveX, y - tearMoveY);
     context.lineTo(x + tearLineX2, y + tearLineY);
-    stroke(context);
+    draw(context, "stroke");
 
-    close(context);
-    begin(context);
+    draw(context, "closePath");
+    draw(context, "beginPath");
 
     context.arc(
       x + tearCircleX,
@@ -287,95 +243,69 @@ function tear(context, size, x, y, position) {
       circleStart,
       3.2
     );
-    stroke(context);
+    draw(context, "stroke");
   }
 
-  close(context);
+  draw(context, "closePath");
 }
 
-const emojiConfigs = {
-  1: (context, size, x, y) => {
-    eyes(context, size, x, y, "eye", "left");
+const emojiData = {
+  face: {
+    id: null,
+    configs: [(context, size, x, y) => drawEmojiOutline(context, size, x, y)],
   },
-
-  2: (context, size, x, y) => {
-    eyes(context, size, x, y, "eye", "right");
+  leftEye: {
+    id: null,
+    configs: [
+      (context, size, x, y) => eyes(context, size, x, y, "eye", "left"),
+      (context, size, x, y) => eyes(context, size, x, y, "blink", "left"),
+      (context, size, x, y) => eyes(context, size, x, y, "sad", "left"),
+      (context, size, x, y) => eyes(context, size, x, y, "half", "left"),
+      (context, size, x, y) => eyes(context, size, x, y, "extraSad", "left"),
+    ],
   },
-
-  3: (context, size, x, y) => {
-    eyes(context, size, x, y, "blink", "left");
+  rightEye: {
+    id: null,
+    configs: [
+      (context, size, x, y) => eyes(context, size, x, y, "eye", "right"),
+      (context, size, x, y) => eyes(context, size, x, y, "blink", "right"),
+      (context, size, x, y) => eyes(context, size, x, y, "sad", "right"),
+      (context, size, x, y) => eyes(context, size, x, y, "half", "right"),
+      (context, size, x, y) => eyes(context, size, x, y, "extraSad", "right"),
+    ],
   },
-
-  4: (context, size, x, y) => {
-    eyes(context, size, x, y, "blink", "right");
+  mouth: {
+    id: null,
+    configs: [
+      (context, size, x, y) => mouth(context, size, x, y, "smile"),
+      (context, size, x, y) => mouth(context, size, x, y, "sad"),
+      (context, size, x, y) => mouth(context, size, x, y, "happy"),
+      (context, size, x, y) => mouth(context, size, x, y, "blank"),
+      (context, size, x, y) => mouth(context, size, x, y, "o"),
+    ],
   },
-
-  5: (context, size, x, y) => {
-    eyes(context, size, x, y, "sad", "left");
+  tougue: {
+    id: null,
+    configs: [
+      (context, size, x, y) => drawTougue(context, size, x, y, "straight"),
+      (context, size, x, y) => drawTougue(context, size, x, y, "side"),
+    ],
   },
-
-  6: (context, size, x, y) => {
-    eyes(context, size, x, y, "sad", "right");
+  leftLaughTear: {
+    id: null,
+    configs: [(context, size, x, y) => laughTear(context, size, x, y, "left")],
   },
-
-  7: (context, size, x, y) => {
-    eyes(context, size, x, y, "half", "left");
+  rightLaughTear: {
+    id: null,
+    configs: [(context, size, x, y) => laughTear(context, size, x, y, "right")],
   },
-
-  8: (context, size, x, y) => {
-    eyes(context, size, x, y, "half", "right");
+  leftTear: {
+    id: null,
+    configs: [(context, size, x, y) => tear(context, size, x, y, "left")],
   },
-
-  9: (context, size, x, y) => {
-    eyes(context, size, x, y, "extraSad", "left");
-  },
-
-  10: (context, size, x, y) => {
-    eyes(context, size, x, y, "extraSad", "right");
-  },
-
-  11: (context, size, x, y) => {
-    mouth(context, size, x, y, "smile");
-  },
-
-  12: (context, size, x, y) => {
-    mouth(context, size, x, y, "sad");
-  },
-
-  13: (context, size, x, y) => {
-    mouth(context, size, x, y, "happy");
-  },
-
-  14: (context, size, x, y) => {
-    mouth(context, size, x, y, "blank");
-  },
-
-  15: (context, size, x, y) => {
-    mouth(context, size, x, y, "o");
-  },
-
-  16: (context, size, x, y) => {
-    tougue(context, size, x, y);
-  },
-
-  17: (context, size, x, y) => {
-    sidetougue(context, size, x, y);
-  },
-
-  18: (context, size, x, y) => {
-    laughTear(context, size, x, y, "left");
-  },
-
-  19: (context, size, x, y) => {
-    laughTear(context, size, x, y, "right");
-  },
-
-  20: (context, size, x, y) => {
-    tear(context, size, x, y, "left");
-  },
-
-  21: (context, size, x, y) => {
-    tear(context, size, x, y, "right");
+  rightTear: {
+    id: null,
+    configs: [(context, size, x, y) => tear(context, size, x, y, "right")],
   },
 };
 
@@ -387,7 +317,7 @@ var emojiSizeSpace = size * space;
 var startX = size * (space / 2);
 var startY = size * (space / 2);
 
-function drawEmoji(size, canvas, id) {
+function drawEmoji(size, canvas, id, type, storeIds) {
   emojiSizeSpace = size * space;
 
   startX = size * (space / 2);
@@ -402,37 +332,68 @@ function drawEmoji(size, canvas, id) {
   context.lineCap = "round";
   context.lineWidth = "3";
 
-  drawEmojiOutline(context, size, startX, startY);
-
-  if (emojiConfigs[id]) {
-    emojiConfigs[id](context, size, startX, startY);
+  if (storeIds) {
+    emojiData[type].id = id;
+    getContentIdsAndDraw(context, size, startX, startY);
+  } else if (emojiData[type] && emojiData[type].configs[id]) {
+    emojiData[type].configs[id](context, size, startX, startY);
   }
 }
 
-var rows = Object.entries(emojiConfigs).length;
-var id = 0;
-
-for (let row = 0; row < rows; row++) {
-  id++;
-
-  var canvas = document.createElement("canvas");
-  drawEmoji(size, canvas, id);
-
-  var link = document.createElement("a");
-  link.href = "#";
-  link.setAttribute("id", id);
-  link.innerHTML = "Add";
-
-  link.onclick = function (event) {
-    canvas = document.querySelector("#emocanvas");
-    drawEmoji(size, canvas, id);
-  };
-
-  var division = document.createElement("div");
-  division.classList = "emodiv";
-  division.appendChild(canvas);
-  division.appendChild(link);
-
-  var container = document.querySelector(".container");
-  container.appendChild(division);
+function getContentIdsAndDraw(context, size, startX, startY) {
+  Object.entries(emojiData).forEach(([type, emojiData]) => {
+    if (emojiData.id !== null) {
+      let id = emojiData.id;
+      emojiData.configs[id](context, size, startX, startY);
+    }
+  });
 }
+
+var canvasDisplay = document.querySelector("#canvasdisplay");
+
+function callFeatures(emojiList, type) {
+  const totalRows = emojiList.length;
+
+  for (let rowIndex = 0; rowIndex < totalRows; rowIndex++) {
+    // Create and configure the canvas for the emoji
+    const emojiCanvas = document.createElement("canvas");
+    emojiCanvas.setAttribute("type", type);
+
+    drawEmoji(size, emojiCanvas, rowIndex, type, false);
+
+    // Create a clickable link for adding the emoji
+    const addLink = document.createElement("a");
+    addLink.href = "#";
+    addLink.setAttribute("id", rowIndex);
+    addLink.setAttribute("type", type);
+    addLink.textContent = "Add";
+
+    addLink.onclick = function (event) {
+      const clickedId = parseInt(event.target.id, 10);
+      const clickedType = event.target.type;
+
+      emojiSize = 150;
+
+      const mainCanvas = document.querySelector("#canvas");
+      canvasDisplay.appendChild(mainCanvas);
+
+      drawEmoji(emojiSize, mainCanvas, clickedId, clickedType, true);
+    };
+
+    // Create a container for the emoji and the link
+    const emojiContainer = document.createElement("div");
+    emojiContainer.classList.add("emojiDivision");
+
+    emojiContainer.appendChild(emojiCanvas);
+    emojiContainer.appendChild(addLink);
+
+    // Append the container to the appropriate section
+    const containerElement = document.querySelector(`#${type}-container`);
+    containerElement.appendChild(emojiContainer);
+  }
+}
+
+// Dynamically call `callFeatures` for each emoji configuration
+Object.entries(emojiData).forEach(([type, emojiData]) => {
+  callFeatures(emojiData.configs, type);
+});
